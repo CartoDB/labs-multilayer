550bf0ab9dd31dfcc3f1a13b99bfe5a941cfa6c1
Dani Carrion <daniel@cartodb.com>
2015-02-06 12:19:42 +0100

    initial release

---
+++ multilayer.js
@@ -0,0 +1,56 @@
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null
+}
+
+var user = getURLParameter('u');
+var table = getURLParameter('t');
+var baseVizJsonUrl = getURLParameter('v');
+
+var multilayer = angular.module('multilayer', []);
+multilayer.controller('SelectorCtrl', function ($scope) {
+    var cartodbLayers = [];
+
+    $scope.selectedLayers = [];
+
+    $scope.layersUpdated = function (id) {
+        var sublayer = cartodbLayers[id];
+        if ($scope.selectedLayers[id]) {
+            sublayer.show();
+        } else {
+            sublayer.hide();
+        }
+    };
+
+    cartodb.createVis('map', baseVizJsonUrl, {
+        zoom: 3,
+        center_lat: 5,
+        center_lon: 0,
+        loaderControl: false,
+        zoomControl: false
+    }).done(function (vis) {
+        var map = vis.getNativeMap();
+
+        var sql = new cartodb.SQL({user: user});
+        sql.execute("SELECT name, viz_json FROM " + table + " WHERE name IS NOT NULL")
+            .done(function (data) {
+                $scope.layers = data.rows;
+                for (var id = 0; id < $scope.layers.length; ++id) {
+                    layer = $scope.layers[id];
+                    layer.id = id;
+                    $scope.selectedLayers[id] = true;
+                    cartodb.createLayer(map, layer.viz_json)
+                        .addTo(map)
+                        .done(function (layer) {
+                            cartodbLayers.push(layer);
+                        })
+                        .error(function (error) {
+                            console.log("error: " + error);
+                        });
+                }
+                $scope.$apply();
+            })
+            .error(function (errors) {
+                console.log("errors: " + errors);
+            });
+    });
+});
